8576946997812abd468518ec18f5e15487598d45
RavgoYT <88573982+RavgoYT@users.noreply.github.com>
2022-01-14 09:40:54 -0800

    For Flow Field

--- shapes.js
+++ shapes.js
@@ -1,68 +1,68 @@
-// Symmetry corresponding to the number of reflections. Change the number for different number of reflections 
-let symmetry = 6;   
+var points = []
+var mult = 0.005
 
-let angle = 360 / symmetry;
-let saveButton, clearButton, mouseButton, keyboardButton;
-let slider;
+var r1
+var r2
+var g1
+var g2
+var b1
+var b2
 
-function setup() { 
-  createCanvas(710, 710);
-  angleMode(DEGREES);
-  background(127);
-
-  // Creating the save button for the file
-  saveButton = createButton('save');
-  saveButton.mousePressed(saveFile);
-
-  // Creating the clear screen button
-  clearButton = createButton('clear');
-  clearButton.mousePressed(clearScreen);
-
-  // Creating the button for Full Screen
-  fullscreenButton = createButton('Full Screen');
-  fullscreenButton.mousePressed(screenFull);
-
-  // Setting up the slider for the thickness of the brush
-  brushSizeSlider = createButton('Brush Size Slider');
-  sizeSlider = createSlider(1, 32, 4, 0.1);
-}
-
-// Save File Function
-function saveFile() {
-  save('design.jpg');
-}
-
-// Clear Screen function
-function clearScreen() {
-  background(127);
-}
-
-// Full Screen Function
-function screenFull() {
-  let fs = fullscreen();
-  fullscreen(!fs);
-}
-
-function draw() {
-  translate(width / 2, height / 2);
-
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-    let mx = mouseX - width / 2;
-    let my = mouseY - height / 2;
-    let pmx = pmouseX - width / 2;
-    let pmy = pmouseY - height / 2;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(30)
+  angleMode(DEGREES)
+  noiseDetail(1)
+  
+  var density = 30
+  var space = width / density
+  
+  for (var x = 0; x < width; x += space) {
     
-    if (mouseIsPressed) {
-      for (let i = 0; i < symmetry; i++) {
-        rotate(angle);
-        let sw = sizeSlider.value();
-        strokeWeight(sw);
-        line(mx, my, pmx, pmy);
-        push();
-        scale(1, -1);
-        line(mx, my, pmx, pmy);
-        pop();
-      }
+    for (var y = 0; y < height; y += space) {
+       var p = createVector(x + random(-10, 10), y + random(-10, 10))
+       points.push(p)
     }
   }
+  
+  r1 = random(255)
+  r2 = random(255)
+  g1 = random(255)
+  g2 = random(255)
+  b1 = random(255)
+  b2 = random(255)
+  
+  mult = random(0.002, 0.01)
+  
+}
+
+
+function draw() {
+  noStroke()
+  
+  
+  for (var i = 0; i < points.length; i++) {
+    
+    var r = map(points[i].x, 0, width, r1, r2)
+    var g = map(points[i].y, 0, height, g1, g2)
+    var b = map(points[i].x, 0, width, b1, b2)
+    var alpha = map(dist(width / 2, height / 2, points[i].x, points[i].y), 0, 350, 400, 0)
+    
+    fill(r, g, b, alpha)
+    
+    var angle = map(noise(points[i].x * mult, points[i].y) * mult, 0, 1, 0, 720)
+    
+    points[i].add(createVector(cos(angle), sin(angle)))
+    
+    if (dist(width / 2, height / 2, points[i].x, points[i].y) < 350){
+          ellipse(points[i].x, points[i].y, 1)
+    }
+
+  }
+  
+function mouseClicked() {
+  saveCanvas('flowfield', 'png')
+}
+  
+  
 }
